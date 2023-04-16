8c6cd3c5bfd7b0d887db09873584a5c247c35476
lutfi <mlutfiibra@outlook.com>
2023-04-16 21:25:38 +0700

    Finishing movies page

---
+++ components/form/radio.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="flex items-center justify-between" v-for="(datum, index) in data" :value="datum.key" :key="index">
+      <label :for="datum.key" class="mr-3">
+        {{ datum.name }}
+      </label>
+      <input :id="datum.key" type="checkbox" class="form-checkbox h-5 w-5 text-indigo-600 order-last">
+    </div>
+  </div>
+</template>
+  
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.options {
+  background-color: rgb(72, 72, 72);
+  border-radius: 4px;
+  padding: 5px 10px;
+
+  &:checked {
+    background: #E74C3C;
+    color:#fff;
+  }
+}
+</style>
